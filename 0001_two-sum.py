0410dec6cc0f0ec90c3cc79a2406653017d27021
tmm90 <tanmiaomiao90@126.com>
2020-08-17 03:16:16 -0400

    add problem 0001.

---
+++ 0001_two-sum.py
@@ -0,0 +1,34 @@
+# -*- encoding=utf-8 -*-
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_set = set()
+        for num in nums:
+            if target - num in nums_set:
+                return [num, target - num]
+
+            nums_set.add(num)
+
+        return []
+
+
+def test_0001():
+    nums = [2, 7, 11, 15]
+    target = 9
+    solution = Solution()
+
+    # 存在情况
+    assert sum(solution.twoSum(nums, target)) == target
+
+    # 不存在情况
+    target = 20
+    assert solution.twoSum(nums, target) == []
+
+    # 边界情况：空数组
+    assert solution.twoSum([], target) == []
---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+使用pylint检查代码格式。pylintrc 参考自 https://gist.github.com/xen/6334976
+
+使用[pytest](https://github.com/pytest-dev/pytest)执行测试用例。
+
+使用travis CI执行进行代码检查与测试用例的执行。
